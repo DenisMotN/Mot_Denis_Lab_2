0732ef2dc180dce0d9d5d65a0f85f7c11467b4ec
denis.mot211001@gmail.com <denis.mot211001@gmail.com>
2022-11-09 11:38:33 +0200

    Lab_4 Done

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Mot_Denis_Lab_2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; internal set; }
+    }
+}
+
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Mot_Denis_Lab_2.Data;
 using Mot_Denis_Lab_2.Models;
+using Mot_Denis_Lab_2.Models.ViewModels;
 
 namespace Mot_Denis_Lab_2.Pages.Categories
 {
@@ -21,12 +22,36 @@ namespace Mot_Denis_Lab_2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
+        {
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                .ThenInclude(i => i.Book)
+                .ThenInclude(i => i.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if(id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                    .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
+
+            }    
+        }
+        /*public async Task OnGetAsync()
         {
             if (_context.Category != null)
             {
                 Category = await _context.Category.ToListAsync();
             }
-        }
+        }*/
     }
 }
